b91870e044e7df5d4a45e39fc40e9fb5ea4f68ce
Dave Block <dave@northcreek.ca>
2017-05-11 18:05:26 -0600

    smsOptIn needs to default to true

--- app/Http/Controllers/FormResponseController.php
+++ app/Http/Controllers/FormResponseController.php
@@ -83,6 +83,9 @@ class FormResponseController extends Controller
                 }
                 //Brix is using customDate2 for birthdate despite the presence of the dateOfBirth field
                 $candidate->set("customDate2", $candidate->get("dateOfBirth"));
+                //Brix and AGS need smsOptIn to be set to true for all candidates
+                $candidate->set("smsOptIn", "true");
+                
                 $retval = $bc->submit($candidate);
                 if (array_key_exists("errorMessage", $retval)) {
                     $data['errormessage']['message'] = $retval['errorMessage'];
